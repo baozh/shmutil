fc46b2e7f4c4e8eacd67edc6689bfccb5ef7bae1
BaoZenghui <bao_z_h@163.com>
2017-05-16 11:39:44 +0800

    inline some current_thread functions.

--- shmutil/current_thread.cc
+++ shmutil/current_thread.cc
@@ -10,29 +10,6 @@ namespace CurrentThread
     __thread char t_tidString[32];
     __thread int t_tidStringLength = 6;
 
-    pid_t gettid()
-    {
-        return static_cast<pid_t>(::syscall(SYS_gettid));
-    }
-
-    void cacheTid()
-    {
-        if (t_cachedTid == 0)
-        {
-            t_cachedTid = gettid();
-            t_tidStringLength = snprintf(t_tidString, sizeof t_tidString, "%5d ", t_cachedTid);
-        }
-    }
-
-    int tid()
-    {
-        if (__builtin_expect(t_cachedTid == 0, 0))
-        {
-            cacheTid();
-        }
-        return t_cachedTid;
-    }
-
     const char* tidString() // for logging
     {
         return t_tidString;
@@ -41,18 +18,5 @@ namespace CurrentThread
     int tidStringLength() // for logging
     {
         return t_tidStringLength;
-    }
-
-    bool isMainThread()
-    {
-        return tid() == ::getpid();
-    }
-
-    void sleepUsec(int64_t usec)
-    {
-        struct timespec ts = {0, 0};
-        ts.tv_sec = static_cast<time_t>(usec / 1000000);
-        ts.tv_nsec = static_cast<long>(usec % 1000000 * 1000);
-        ::nanosleep(&ts, NULL);
     }
 };
--- shmutil/current_thread.h
+++ shmutil/current_thread.h
@@ -11,19 +11,46 @@
 #include <sys/syscall.h>
 #include <sys/types.h>
 #include <stdint.h>
-#include <time.h>
+
 
 #include <stdio.h>
 
 namespace CurrentThread
 {
-    pid_t gettid();
-    void cacheTid();
-    int tid();
+    extern __thread int t_cachedTid;
+    extern __thread char t_tidString[32];
+    extern __thread int t_tidStringLength;
+
+    inline pid_t gettid()
+    {
+        return static_cast<pid_t>(::syscall(SYS_gettid));
+    }
+
+    inline void cacheTid()
+    {
+        if (t_cachedTid == 0)
+        {
+            t_cachedTid = gettid();
+            t_tidStringLength = snprintf(t_tidString, sizeof t_tidString, "%5d ", t_cachedTid);
+        }
+    }
+
+    inline int tid()
+    {
+        if (__builtin_expect(t_cachedTid == 0, 0))
+        {
+            cacheTid();
+        }
+        return t_cachedTid;
+    }
+
     const char* tidString(); // for logging
     int tidStringLength(); // for logging
-    bool isMainThread();
-    void sleepUsec(int64_t usec);
+
+    inline bool isMainThread()
+    {
+        return tid() == ::getpid();
+    }
 };
 
 #endif
